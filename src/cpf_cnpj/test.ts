7dcc1f460a92af5d6fab249466d19248001014f0
Roberto Seba <roberto@robertoseba.com>
2023-03-08 08:02:33 -0300

    [FIX] apaga arquivo morto

--- src/cpf_cnpj/test.ts
+++
@@ -1,7 +0,0 @@
-import { CpfCnpjHelper } from '.'
-
-console.log(CpfCnpjHelper.geraCpf().formatado)
-
-console.log(CpfCnpjHelper.geraCnpj().formatado)
-
-const a = new CpfCnpjHelper('091.016.557-28')
